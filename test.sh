253f1adbe5db6cdf3c0f276be68f52966be6964c
Tim Cameron Ryan <id@timryan.org>
2014-04-03 01:38:22 -0400

    Adds compilation changes to support cortex-m3 compilation.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd luajit
-make CROSS="arm-none-eabi-" TARGET_CFLAGS="-mcpu=cortex-m3  -mfloat-abi=softfp"
+make CROSS="arm-none-eabi-" TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp"
