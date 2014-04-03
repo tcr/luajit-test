1c563698a6879f1f9ccb552bba91bb052a133b44
Tim Cameron Ryan <id@timryan.org>
2014-04-03 09:45:24 -0400

    Adds duplicate information everywhere for thumb, breaks on DASM error.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd luajit
-make HOST_CC="gcc -m32" CROSS="arm-none-eabi-" TARGET_SYS=Other TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp"
+make HOST_CC="gcc-4.9 -m32" CROSS="arm-none-eabi-" TARGET_SYS=Other TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp"
