70fbd3f24643ad86e57770b5fd8c74469670867a
Tim Cameron Ryan <id@timryan.org>
2014-04-03 01:43:16 -0400

    EVERYTHING WENT SwIMMINGLY with compilation.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd luajit
-make CROSS="arm-none-eabi-" TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp"
+make HOST_CC="gcc -m32" CROSS="arm-none-eabi-" TARGET_SYS=Other TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp"
