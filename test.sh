52652eca6828f22d69bb78dddfaaad9893db9757
Tim Cameron Ryan <id@timryan.org>
2014-06-05 23:58:48 -0700

    Avoids building against machasm target

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd luajit
-make HOST_CC="gcc-4.9 -m32" CROSS="arm-none-eabi-" TARGET_SYS=Other TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp"
+# make clean
+make HOST_CC="gcc-4.9 -m32" CROSS="arm-none-eabi-" TARGET_SYS=Other TARGET_CFLAGS="-mcpu=cortex-m3 -mthumb -mfloat-abi=softfp" TARGET_SYS=arm
