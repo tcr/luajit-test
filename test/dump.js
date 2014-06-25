15809bde48c3fa1ff7faa7eb82e7a0836dee8d42
Tim Cameron Ryan <id@timryan.org>
2014-06-25 03:14:05 -0700

    Corrects test suite for gcc-arm, and compilation for b.n.

--- test/dump.js
+++ test/dump.js
@@ -13,17 +13,14 @@ function collect (fn) {
 var name = process.argv[2] || 'test';
 var showasm = !!process.argv[3]
 
-var ret = spawn('arm-none-eabi-objdump', ['-D', 'main']);
+var ret = spawn('arm-none-eabi-objdump', ['-D', 'main', '-Mforce-thumb']);
 ret.stdout.pipe(collect(function (buf) {
 	buf = String(buf)
 	var i = 0;
 	buf.match(new RegExp(name + ">:\\s*\\n([\\s\\S]*?)\\n\\n"))[1].split(/\n/).map(function (out) {
-		out.substr(10, 11).split(/\s+/).filter(function (a) {
-			return a;
-		}).forEach(function (line) {
-			console.log(';;;', line + (showasm ? '\t#' + out.substr(21) : ''))
-			// console.log(line)
-			out = '';
-		})
+		var line = out.substr(10).split(/\s{2,}/)
+		// console.log(';;;', line + (showasm ? '\t#' + out.substr(21) : ''))
+		console.log(';;;', line.shift(), '\t#', line.join(' '));
+	// 	out = '';
 	})
 }))
